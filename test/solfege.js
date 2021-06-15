6bf59f8b4156fb95e42835e2535c865e210fe907
Katerina Paltoglou <katpaltoglou@gmail.com>
2021-06-15 12:09:58 +0300

    update branch with transformed code

--- test/solfege.js
+++ test/solfege.js
@@ -1,64 +1,64 @@
-var vows = require('vows'),
-    assert = require('assert'),
-    teoria = require('../');
+import ext_vows_vows from "vows";
+import ext_assert_assert from "assert";
+import { note as indexjs_note, scale as indexjs_scale } from "../";
 
-vows.describe('Solfege').addBatch({
+ext_vows_vows.describe('Solfege').addBatch({
   'C in C minor': function() {
-    var note = teoria.note('c');
-    assert.equal(note.solfege(teoria.scale(note, 'minor')), 'do');
+    var note = indexjs_note('c');
+    ext_assert_assert.equal(note.solfege(indexjs_scale(note, 'minor')), 'do');
   },
 
   'A in d major': function() {
-    var note = teoria.note('a');
-    var tonic = teoria.note('d');
-    assert.equal(note.solfege(teoria.scale(tonic, 'major')), 'so');
+    var note = indexjs_note('a');
+    var tonic = indexjs_note('d');
+    ext_assert_assert.equal(note.solfege(indexjs_scale(tonic, 'major')), 'so');
   },
 
   'F# in B major': function() {
-    var note = teoria.note('f#');
-    var tonic = teoria.note('B');
-    assert.equal(note.solfege(teoria.scale(tonic, 'major')), 'so');
+    var note = indexjs_note('f#');
+    var tonic = indexjs_note('B');
+    ext_assert_assert.equal(note.solfege(indexjs_scale(tonic, 'major')), 'so');
   },
 
   'C4 in C4 minor': function() {
-    var note = teoria.note('c4');
-    var scale = teoria.scale(note, 'minor');
-    assert.equal(note.solfege(scale, true), 'do');
+    var note = indexjs_note('c4');
+    var scale = indexjs_scale(note, 'minor');
+    ext_assert_assert.equal(note.solfege(scale, true), 'do');
   },
 
   'A3 in D4 major': function() {
-    var note = teoria.note('a3');
-    var scale = teoria.scale('d4', 'major');
-    assert.equal(note.solfege(scale, true), 'so,');
+    var note = indexjs_note('a3');
+    var scale = indexjs_scale('d4', 'major');
+    ext_assert_assert.equal(note.solfege(scale, true), 'so,');
   },
 
   'F#6 in B5 major': function() {
-    var note = teoria.note('f#6');
-    var scale = teoria.scale('b5', 'major');
-    assert.equal(note.solfege(scale, true), 'so');
+    var note = indexjs_note('f#6');
+    var scale = indexjs_scale('b5', 'major');
+    ext_assert_assert.equal(note.solfege(scale, true), 'so');
   },
 
   'F2 in E6 phrygian': function() {
-    var note = teoria.note('f2');
-    var scale = teoria.scale('e6', 'phrygian');
-    assert.equal(note.solfege(scale, true), 'ra,,,,');
+    var note = indexjs_note('f2');
+    var scale = indexjs_scale('e6', 'phrygian');
+    ext_assert_assert.equal(note.solfege(scale, true), 'ra,,,,');
   },
 
   'Eb10 in E8 dorian': function() {
-    var note = teoria.note('eb10');
-    var scale = teoria.scale('e8', 'dorian');
-    assert.equal(note.solfege(scale, true), 'de\'\'');
+    var note = indexjs_note('eb10');
+    var scale = indexjs_scale('e8', 'dorian');
+    ext_assert_assert.equal(note.solfege(scale, true), 'de\'\'');
   },
 
   'A#6 in Bb4 locrian': function() {
-    var note = teoria.note('A#6');
-    var scale = teoria.scale('Bb4', 'locrian');
-    assert.equal(note.solfege(scale, true), 'tai\'');
+    var note = indexjs_note('A#6');
+    var scale = indexjs_scale('Bb4', 'locrian');
+    ext_assert_assert.equal(note.solfege(scale, true), 'tai\'');
   },
 
   'E2 in C3 major': function() {
-    var note = teoria.note('e2');
-    var scale = teoria.scale('c3', 'major');
-    assert.equal(note.solfege(scale, true), 'mi,');
+    var note = indexjs_note('e2');
+    var scale = indexjs_scale('c3', 'major');
+    ext_assert_assert.equal(note.solfege(scale, true), 'mi,');
   }
 }).export(module);
